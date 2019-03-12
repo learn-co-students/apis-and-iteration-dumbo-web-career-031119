4428dddde33c085b1f8320cee1029280eaa186a1
Daniel Ko <dankomong@gmail.com>
2019-03-12 13:22:06 -0400

    Finish

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -16,10 +16,48 @@ def get_character_movies_from_api(character_name)
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
+  movies_arr = []
+  movies_hash_arr = []
+  response_hash.each do |key, value|
+    if key == "results"
+      value.each do |info|
+        if info["name"].downcase == character_name
+          movies_arr.concat(info["films"])
+        end
+      end
+    end
+  end
+
+  # Now making web requests from the movie urls
+  movies_arr.each do |film|
+    film_response = RestClient.get(film)
+    film_hash = JSON.parse(film_response)
+    movies_hash_arr.push(film_hash)
+  end
+  movies_hash_arr
+  # binding.pry
+  # 0
 end
+
+# get_character_movies_from_api("Luke skywalker")
 
 def print_movies(films)
   # some iteration magic and puts out the movies in a nice list
+  films.each_with_index do |hash, index|
+    puts "#{index + 1}. Title: #{hash['title']}"
+    # There's prob a director and producer for every movie but making sure just in case
+    if !hash["director"]
+      puts "Director: No Director"
+    else
+      puts "Director(s): #{hash['director']}"
+    end
+    if !hash["producer"]
+      puts "Producer: No Producer"
+    else
+      puts "Producer(s): #{hash['producer']}"
+    end
+    puts "Release Date: #{hash['release_date']}"
+  end
 end
 
 def show_character_movies(character)
